64ff8f319ba88a92258731645aa632e05a91ba7c
Glitch (books-app) <none>
2020-05-02 04:38:49 +0000

    Books Manager App

--- server.js
+++ server.js
@@ -1,8 +1,3 @@
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
@@ -10,81 +5,91 @@ const low = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
 const shortid = require("shortid");
 
-const adapter = new FileSync("db.json");
-const db = low(adapter);
+app.use(express.static("public"));
 
 app.set("view engine", "/views");
 app.set("view engine", "pug");
 
+const adapter = new FileSync("db.json");
+const db = low(adapter);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-db.defaults({ todos: [] }).write();
+db.defaults({ books: [] }).write();
 
-// https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (req, res) => {
-  res.render("index", {
-    text: "I love CodersX"
+  res.render("index");
+});
+
+app.get("/books", (req, res) => {
+  res.render("books/index", {
+    books: db.get("books").value()
   });
 });
 
-app.get("/todos", (req, res) => {
-  const q = req.query.q;
-  if (!q) {
-    res.render("todos/index", {
-      todos: db.get("todos").value()
-    });
-  } else {
-    const matchTodos = db
-      .get("todos")
-      .value()
-      .filter(todo => todo.todo.toLowerCase().indexOf(q.toLowerCase()) !== -1);
-
-    res.render("todos/index", {
-      todos: matchTodos
-    });
-  }
+app.get("/books/create", (req, res) => {
+  res.render("books/create");
 });
 
-app.get("/todos/create", (req, res) => {
-  res.render("todos/create");
-});
-
-app.post("/todos/create", (req, res) => {
+app.post("/books/create", (req, res) => {
   req.body.id = shortid.generate();
-  db.get("todos")
+  db.get("books")
     .push(req.body)
     .write();
-  res.redirect("/todos");
+  res.redirect("/books");
 });
 
-app.get("/todos/:id", (req, res) => {
+app.get("/books/:id", (req, res) => {
   const id = req.params.id;
   const name = db
-    .get("todos")
+    .get("books")
     .find({ id })
     .value();
-  res.render("todos/view", {
+  res.render("books/view", {
     name
   });
 });
 
-app.get("/todos/:id/delete", (req, res) => {
-  console.log(req.params);
+app.get("/books/edit/:id", (req, res) => {
   const id = req.params.id;
-  const name = db
-    .get("todos")
+  const book = db.get("books").find({ id }).value();
+  res.render("books/edit", {
+    book
+  });
+});
+
+app.get("/books/delete/:id", (req, res) => {
+  const id = req.params.id;
+  const book = db
+    .get("books")
     .find({ id })
     .value();
-  if (name) {
-    db.get("todos").remove({ todo: name.todo }).write();
-    res.render("todos/index", {
-      todos: db.get("todos").value()
+  if (book) {
+    db.get("books")
+      .remove({ name: book.name })
+      .write();
+    res.render("books/index", {
+      books: db.get("books").value()
     });
   }
 });
 
+app.post("/books/edit/:id", (req, res) => {
+  const id = req.params.id;
+  const book = db
+    .get("books")
+    .find({ id })
+    .assign({
+      name: req.body.name,
+      description: req.body.description
+    })
+    .write();
+    console.log('books', book)
+    res.redirect("/books");
+});
+
 // listen for requests :)
-app.listen(process.env.PORT, () => {
-  console.log("Server listening on port " + process.env.PORT);
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
 });
