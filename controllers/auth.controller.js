5d8083260c989695e78b058f1679db8a7535619f
Glitch (alert-wrong-login) <none>
2020-05-09 09:05:58 +0000

    check wrong login more times

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,40 +1,50 @@
-const md5 = require("md5");
+// const md5 = require("md5");
+const bcrypt = require("bcrypt");
 const db = require("../db");
 
 
 module.exports.login = (req, res) => {
   res.render("auth/login");
 }
+let wrongLoginCount = 0;
 
-module.exports.postLogin = (req, res, next) => {
+module.exports.postLogin = async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   
   const user = db.get("users").find({ email }).value();
   
   if(!user) {
+    wrongLoginCount++;
     res.render("auth/login", {
       errors: [
         "User does not exists"
       ],
-      values: req.body
+      values: req.body,
+      loginCount: wrongLoginCount
     });
     return;
   }
   
-  const hardPassword = md5(password);
-  console.log("ps", hardPassword);
+  // const passwordHash = await bcrypt.hash(user.password, 6);
+  // console.log('user', passwordHash)
+
   
-  if(user.password !== hardPassword) {
+  const match = await bcrypt.compare(password, user.password);
+  
+  if(!match) {
+    wrongLoginCount++;
     res.render("auth/login", {
       errors: [
         "Wrong password!"
       ],
-      values: req.body
+      values: req.body,
+      loginCount: wrongLoginCount
     });
     return;
   }
   
+  wrongLoginCount = 0;
   
   res.cookie("userId", user.id);
   res.redirect("/users")
